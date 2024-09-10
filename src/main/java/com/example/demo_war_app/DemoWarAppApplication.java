510a0fc4f8f067cbccf6a001c2966862ee442287
Mukut Bhattacharjee <mukut.bhattacharjee@tesco.com>
2024-09-10 13:50:47 +0530

    first commit

---
+++ src/main/java/com/example/demo_war_app/DemoWarAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_war_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWarAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_war_app/DemoWarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_war_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWarAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
